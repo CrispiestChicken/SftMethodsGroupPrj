c9dedaf5f262763fca83646f3db83f3611f2d4ef
40582229 <40582229@live.napier.ac.uk>
2024-11-21 17:55:36 +0000

    fixing unit tests

---
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -0,0 +1,69 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppIntegrationTest {
+    static App app;
+    static City city;
+    static Capital_City capitalcity;
+    @BeforeAll
+    static void init() {
+        app = new App();
+        app.connect("localhost:33060", 30000);
+    }
+
+
+    /**
+     * Tests if the data collected from the database is what it's meant to be
+     * for getting citties
+     */
+    @Test
+    void testGetCity(){
+        ArrayList<City> testCity = app.getCity();
+        assertTrue(testCity.get(0).name.equals("Kabul"));
+    }
+
+    /**
+     * Tests if the data collected from the database is what it's meant to be
+     * for the top populateed cities in a dirtrict
+     */
+    @Test
+    void testGetTopPopulateCitiesInADistrict(){
+        ArrayList<City> cityPopulationInADistrict = app.getTopPopulateCitiesInADistrict(2, "Zuid-Holland");
+        assertTrue(cityPopulationInADistrict.get(0).name.equals("Rotterdam"));
+        assertTrue(cityPopulationInADistrict.get(0).district.equals("Zuid-Holland"));
+        assertTrue(cityPopulationInADistrict.get(0).population == 593321);
+
+        assertTrue(cityPopulationInADistrict.get(1).name.equals("Haag"));
+        assertTrue(cityPopulationInADistrict.get(1).district.equals("Zuid-Holland"));
+        assertTrue(cityPopulationInADistrict.get(1).population == 440900);
+    }
+
+    /**
+     * Tests if the data collected from the database is what it's meant to be
+     * for the all cities population descending.
+     */
+    @Test
+    void testCorrectCitiesPopDesc()
+    {
+        ArrayList<City> CitiesPopDesc = app.GetAllCitiesPopDesc();
+        assertEquals(CitiesPopDesc.get(0).name,"Mumbai (Bombay)");
+    }
+
+    /**
+     * Tests if the data collected from the database is what it's meant to be
+     * for the the number of given cities population in desending order.
+     */
+    @Test
+    void testGetGivenNumberOfCapitalCitiesPopDesc()
+    {
+        ArrayList<City> capitalCitiesPopDesc = app.GetGivenNumberOfCapitalCitiesPopDesc(1);
+        assertEquals(capitalCitiesPopDesc.get(0).name,"Seoul");
+    }
+}
--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -2,7 +2,6 @@ package com.napier.sem;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 
 import java.util.ArrayList;
 
@@ -15,56 +14,51 @@ public class AppTest {
     @BeforeAll
     static void init() {
         app = new App();
-        app.connect("localhost:33060", 30000);
+        city = new City();
+        capitalcity = new Capital_City();
     }
 
-
-    /**
-     * Tests if the data collected from the database is what it's meant to be
-     * for getting citties
-     */
     @Test
-    void testGetCity(){
-        ArrayList<City> testCity = app.getCity();
-        assertTrue(testCity.get(0).name.equals("Kabul"));
+    void testCity()
+    {
+        city.name = "Edinburgh";
+        city.countryCode = "SCO";
+        city.district = "NONE";
+        city.population = 10000;
+        String cityName = city.name;
+        //app.displayCity(city);
+        assertTrue(cityName.equals("Edinburgh"));
     }
-
-    /**
-     * Tests if the data collected from the database is what it's meant to be
-     * for the top populateed cities in a dirtrict
-     */
+    /*
     @Test
-    void testGetTopPopulateCitiesInADistrict(){
-        ArrayList<City> cityPopulationInADistrict = app.getTopPopulateCitiesInADistrict(2, "Zuid-Holland");
-        assertTrue(cityPopulationInADistrict.get(0).name.equals("Rotterdam"));
-        assertTrue(cityPopulationInADistrict.get(0).district.equals("Zuid-Holland"));
-        assertTrue(cityPopulationInADistrict.get(0).population == 593321);
+    void testCapitalCity()
+    {
 
-        assertTrue(cityPopulationInADistrict.get(1).name.equals("Haag"));
-        assertTrue(cityPopulationInADistrict.get(1).district.equals("Zuid-Holland"));
-        assertTrue(cityPopulationInADistrict.get(1).population == 440900);
+        capitalcity.name = "Edinburgh";
+        capitalcity.countryCode = "SCO";
+        capitalcity.population = 10000;
+        String cityName = capitalcity.name;
+        app.displayCity(city);
+        assertTrue(cityName.equals("Edinburgh"));
     }
+*/
 
+
+    /*
     /**
      * Tests if the data collected from the database is what it's meant to be
      * for the all cities population descending.
      */
+
+    /*
+    This doesn't work at the moment because I can't connect to the database yet in the tests I will fix this
+    for code review 3.
     @Test
-    void testCorrectCitiesPopDesc()
+    void TestCorrectCitiesPopDesc()
     {
         ArrayList<City> CitiesPopDesc = app.GetAllCitiesPopDesc();
+
         assertEquals(CitiesPopDesc.get(0).name,"Mumbai (Bombay)");
     }
-
-    /**
-     * Tests if the data collected from the database is what it's meant to be
-     * for the the number of given cities population in desending order.
-     */
-    @Test
-    void testGetGivenNumberOfCapitalCitiesPopDesc()
-    {
-        ArrayList<City> capitalCitiesPopDesc = app.GetGivenNumberOfCapitalCitiesPopDesc(1);
-        assertEquals(capitalCitiesPopDesc.get(0).name,"Seoul");
-    }
-
+    */
 }
