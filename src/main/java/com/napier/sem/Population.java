aa14a4a7517989228700af56c96528726f2eccaf
Kyle Read <kread999@gmail.com>
2024-11-25 03:13:51 +0000

    Created the Population.java class

---
+++ src/main/java/com/napier/sem/Population.java
@@ -0,0 +1,16 @@
+package com.napier.sem;
+
+/**
+ * Contains all the details needed for a population report.
+ */
+public class Population
+{
+    public String AreaName;
+    public int PopulationOfArea;
+    public int PopulationOfAreaInCities;
+    public double PopulationOfAreaInCitiesPercent;
+    public int PopulationOfAreaNotInCities;
+    public double PopulationOfAreaNotInCitiesPercent;
+
+
+}
