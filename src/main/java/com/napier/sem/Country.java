91052a65674b87b96a6dfb941ed37b72d7ce2794
Kyle Read <kread999@gmail.com>
2024-11-25 03:09:42 +0000

    Created the Country.java class

---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,14 @@
+package com.napier.sem;
+
+/**
+ * This class stores the details we need for a country report.
+ */
+public class Country
+{
+    public String CountryCode;
+    public String CountryName;
+    public String Continent;
+    public String Region;
+    public String Population;
+    public String CapitalCity;
+}
