0f615a9f445ee279215825620cba838404defe43
CrispyChicken <52355245+CrispiestChicken@users.noreply.github.com>
2024-11-30 14:00:47 +0000

    Made the connection to the database much faster as I noticed it was doing it twice and the first time would take much longer than the second

--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -7,11 +7,9 @@ public class Main {
         // Create new Application
         App a = new App();
 
-        a.connect("localhost:33060", 10000);
-
 
         if (args.length < 1) {
-
+            a.connect("localhost:33060", 10000);
         } else {
             a.connect(args[0], Integer.parseInt(args[1]));
         }
